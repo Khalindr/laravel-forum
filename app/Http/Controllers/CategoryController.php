0458ae7b3ef7a16ea2b07de6044af599bf419ad3
DoreenDaunique <daunique.doreen@gmail.com>
2018-04-12 08:31:49 +0200

    route store category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -43,7 +43,7 @@ class CategoryController extends Controller
 
         $category->save();
 
-        return redirect()->route('category.index');
+        return redirect()->route('home');
     }
 
     /**
