991c53dd34f3d635df80e95ab76d4982d4367813
PandarkMeow <marbasdu10@gmail.com>
2018-04-12 08:30:23 +0200

    fix vue show messages

--- resources/views/message/show.blade.php
+++ resources/views/message/show.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div>
-    <h2>{{ $message->titre }}</h2>
-        <p>{{ $message->texte }}</p>
+    <h2>{{ $message->title }}</h2>
+        <p>{{ $message->text }}</p>
         <p>Ecrit par <a href="mailto:{{$message->user->email}}">{{ $message->user->name }}</a></p>
     </div>
 @endsection
