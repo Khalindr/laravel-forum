233f65f19db5959a3c42feb190f9654f0d8d53c1
PandarkMeow <marbasdu10@gmail.com>
2018-04-19 11:19:55 +0200

    ajout commentaires

--- resources/views/message/show.blade.php
+++ resources/views/message/show.blade.php
@@ -11,12 +11,26 @@
 
                     <div class="panel-body">
                         <p>{{ $message->text }}</p>
-                        <p><img src="{{ Storage::url($message->file) }}">sers</p>
+                        <p><img src="{{ Storage::url($message->file) }}"></p>
                         <form method="POST" action="{{ route('messages.destroy', ['id' => $message->id]) }}">
                             {{ csrf_field() }}
                             <input type="hidden" name="_method" value="DELETE">
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
                             <button type="submit">Delete</button>
+                        </form>
+                        <div class="list-group-item">
+                            @foreach($message->comments as $comment)
+                                <div class="list-group-item">
+                                    <h4 class="">Par ...</h4>
+                                    <p>{{ $comment->text }}</p>
+                                </div>
+                            @endforeach
+                        </div>
+                        <form method="POST" action="{{ route('comments.store') }}">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="message_id" value="{{ $message->id }}">
+                            <textarea type="text" name="text" placeholder="Votre commentaire"></textarea>
+                            <button type="submit">Soumettre</button>
                         </form>
                     </div>
 @endsection
