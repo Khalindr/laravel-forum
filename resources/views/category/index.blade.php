9af16d2e124dbcdc5ace41eca792cf937e5d3a4b
Khalindr <elise.fety@gmail.com>
2018-04-19 08:42:47 +0200

    ADD boutons crud dans categories et messages

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,18 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-                    <div class="panel-heading">Toutes les catégories</div>
+                    <div class="panel-heading">
+                        <h2>Toutes les catégories</h2><a href="/categories/create" class="pull-right"><button class="btn btn-primary add">Ajouter</button></a>
+                    </div>
 
 
                         <div class="panel-body">
                             <div class="list-group">
                                 @foreach($categories as $category)
+                                    <div class="buttons-edit">
+                                        <a href="/categories/{{$category->id}}/edit" class=""><button class="btn btn-success">Modifier</button></a>
+                                        <form method="POST" action="{{ route('categories.destroy', ['id' => $category->id]) }}" class="pull-right">
+                                            {{ csrf_field() }}
+                                            <input type="hidden" name="_method" value="DELETE">
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                            <button type="submit" class="btn btn-danger">Supprimer</button>
+                                        </form>
+                                    </div>
                                     <a href="/categories/{{$category->id}}" class="list-group-item">
                                         <h4>{{$category->name}}</h4>
                                     </a>
                                 @endforeach
                             </div>
                         </div>
-
-
 @endsection
